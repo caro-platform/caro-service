6aced1154c95f3e8e3ab5fe3c58830a93e85205c
Alexander Smoktal <cosm.ua@gmail.com>
2024-05-08 12:27:47 +0300

    Temporaly commented the code

--- krossbar/src/lib.rs
+++ krossbar/src/lib.rs
@@ -1,13 +1,13 @@
-pub mod method;
-pub mod peer;
-pub mod service;
-pub mod signal;
-pub mod state;
-pub mod this;
+// pub mod method;
+// pub mod peer;
+// pub mod service;
+// pub mod signal;
+// pub mod state;
+// pub mod this;
 
-pub use krossbar_derive::*;
-pub use method::Method;
-pub use peer::Peer;
-pub use service::Service;
-pub use signal::Signal;
-pub use state::State;
+// pub use krossbar_derive::*;
+// pub use method::Method;
+// pub use peer::Peer;
+// pub use service::Service;
+// pub use signal::Signal;
+// pub use state::State;
